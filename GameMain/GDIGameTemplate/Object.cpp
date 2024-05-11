bf0a027c87a687313c0ed286eff8a7e65a6aa445
bingsub <ojlkp106@naver.com>
2024-05-11 22:06:09 +0900

    콜라이더 업데이트

--- GameMain/GDIGameTemplate/Object.cpp
+++ GameMain/GDIGameTemplate/Object.cpp
@@ -64,8 +64,8 @@ void Object::Render()
 	
 	/*Render::DrawRect((int)m_pos.x - (int)m_renderBounds.extents.x,
 		(int)m_pos.y - (int)m_renderBounds.extents.y,
-		(int)m_pos.x + (int)m_renderBounds.extents.x,
-		(int)m_pos.y + (int)m_renderBounds.extents.y, RGB(0, 255, 0));*/
+		(int)m_renderBounds.extents.x * 2,
+		(int)m_renderBounds.extents.y * 2, RGB(0, 255, 0));*/
 
 	// 애니메이션 리소스가 있고 특정 모션이 설정되어 있으면 해당 프레임을 그린다.
 	if (m_pAnimationResource && m_AnimationMotionIndex != -1)
