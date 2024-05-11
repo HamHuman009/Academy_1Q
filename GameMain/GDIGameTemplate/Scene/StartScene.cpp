91cc5aa327a9be393b359e6e21c34a0a200fb7b9
HamHuman <166357862+HamHuman009@users.noreply.github.com>
2024-05-12 08:55:32 +0900

    검정화면 확인

--- GameMain/GDIGameTemplate/Scene/StartScene.cpp
+++ GameMain/GDIGameTemplate/Scene/StartScene.cpp
@@ -12,10 +12,10 @@ void StartScene::Init()
 	
 }
 
-void StartScene::Render()
+void StartScene::Render() //이 부분 고칠 것.
 {	
 	for (int i = 0; i < m_arrObj->size(); i++) {
-		m_arrObj[i]
+		m_arrObj[i];
 	}
 }
 
