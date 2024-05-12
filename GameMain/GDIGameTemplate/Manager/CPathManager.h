ba3c552308df76b79051d8d2bfc5e849054f490a
secheol_woo <joker1092@nate.com>
2024-05-12 17:01:36 +0900

    pathmanager , resourcemanager add
    pathmanager , resourcemanager add

---
+++ GameMain/GDIGameTemplate/Manager/CPathManager.h
@@ -0,0 +1,12 @@
+#pragma once
+class CPathManager
+{
+private:
+	//경로를 저장할 변수
+	wchar_t contentPath[255];
+public:
+	void Init(); // 초기화
+	const wchar_t* GetContentPath() { return contentPath; } //resource 폴더 경로 반환
+};
+
+
---
+++ GameMain/GDIGameTemplate/Manager/CResourceManager.cpp
@@ -0,0 +1,44 @@
+#include "CResourceManager.h"
+#include "CPathManager.h"
+
+CPathManager path;
+
+CResourceManager::ResourceManager() {
+
+}
+
+CResourceManager::~ResourceManager() {
+}
+
+Gdiplus::Bitmap* CResourceManager::LoadBitmapResouce(const std::wstring& _strkey, const std::wstring& _path)
+{
+	//키값으로 map에서 찾기
+	Gdiplus::Bitmap* pBitmap = FindBitmapResouce(_strkey);
+	//있으면 반환
+	if (pBitmap!=nullptr)
+	{
+		return pBitmap;
+	}
+	//pathmanager로 상대경로 붙이기
+	std::wstring strFilePath = path.GetContentPath();
+	strFilePath += _path;
+	//bitmap 로드
+	Gdiplus::Bitmap* pBitmap = Gdiplus::Bitmap::FromFile(strFilePath.c_str());
+	//map에 저장 하고
+	m_mapBitmap.insert(make_pair(_strkey, pBitmap));
+	//bitmap* 전달
+	return pBitmap;
+}
+
+Gdiplus::Bitmap* CResourceManager::FindBitmapResouce(const std::wstring& _strkey)
+{
+	//map의 이터레이터를 사용 key값으로 찾음
+	std::map<std::wstring, Gdiplus::Bitmap*>::iterator iter = m_mapBitmap.find(_strkey);
+	//iterator 종료시 까지 없는 경우 --> map에 내용물이 없는경우 nullptr 반환
+	if (iter == m_mapBitmap.end())
+	{
+		return nullptr;
+	}
+		//있는 경우 <first,second> -> <key,내용물>이므로 내용물을 반환
+	return iter->second;
+}
