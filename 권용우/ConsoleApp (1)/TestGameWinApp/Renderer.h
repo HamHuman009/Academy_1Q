7c79a27b216535b8ebec0122e0d5d5474dc94880
bingsub <121720685+zmalqp123@users.noreply.github.com>
2024-05-13 09:34:40 +0900

    안쓰는 프레임워크 삭제

--- 권용우/ConsoleApp (1)/TestGameWinApp/Renderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Component.h"
-
-namespace Engine {
-	class Renderer : public Component
-	{
-	};
-}
--- 권용우/ConsoleApp (1)/TestGameWinApp/Transform.cpp
+++
@@ -1,9 +0,0 @@
-#include "Transform.h"
-#include "GameStruct.h"
-
-namespace Engine {
-	Transform::Transform() {
-		position = { 0.0f, 0.0f };
-	}
-
-}
