90d3675ecf4cfe5bea5e5062acd2ce0189e4e113
matstar33 <76931377+matstar33@users.noreply.github.com>
2024-05-13 12:03:43 +0900

    플레이어 오브젝트 생성

--- GameMain/GDIGameTemplate/GameLogic/Objects/Player.cpp
+++ GameMain/GDIGameTemplate/GameLogic/Objects/Player.cpp
@@ -1,33 +1,22 @@
 #include "Player.h"
-
-//void Player::Init()
-//{
-//}
-//
-//void Player::Update(float delta)
-//{
-//}
-//
-//void Player::Render()
-//{
-//}
-//
-//void Player::OnTrigger()
-//{
-//}
+#include "../System/InputSystem.h"
 
 void Player::Init()
 {
+	// 초기화
 }
 
 void Player::Update(float delta)
 {
+	// 플레이어 그리고 인풋을 받기
 }
 
 void Player::Render()
 {
+	// 실질적으로 플레이어 그리기
 }
 
 void Player::OnTrigger()
 {
+	// 물고기 잡을때 트리거
 }
