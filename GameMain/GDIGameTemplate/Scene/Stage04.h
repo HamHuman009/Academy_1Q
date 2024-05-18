4d0b0bc317742f8b80519056c73bb58254f87864
matstar33 <76931377+matstar33@users.noreply.github.com>
2024-05-18 10:12:18 +0900

    수정

--- GameMain/GDIGameTemplate/Scene/Stage04.h
+++ GameMain/GDIGameTemplate/Scene/Stage04.h
@@ -1,7 +1,7 @@
 #pragma once
-class Stage04
-{
-};
+#include "CScene.h"
+//#include "../GameLogic/Objects/Object.h"
+#include "../GameLogic/Objects/Player.h"
 
 class UIImage;
 //class Player;
@@ -9,9 +9,9 @@ class UIBackGround;
 class Stage04 : public CScene
 {
 private:
-	/*UIBackGround* myBack; // ï¿½Ì·ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ m_arrObjï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½.
+	/*UIBackGround* myBack; // ÀÌ·± º¯¼ö´Â m_arrObj¿¡¼­ °ü¸®.
 	Player* m_Player; */
-	//UIBackGround* myBackGround; //fixedUpdate Ã³ï¿½ï¿½ï¿½Ø¾ï¿½ï¿½Ø¼ï¿½ ï¿½Îµï¿½ï¿½ï¿½ï¿½Ï°ï¿½ ï¿½ï¿½ï¿½ï¿½.
+	//UIBackGround* myBackGround;  //fixedUpdate Ã³¸®ÇØ¾ßÇØ¼­ ºÎµæÀÌÇÏ°Ô ¼±¾ð.
 	//UIBackGround* myUPBackGround;
 public:
 	// CSceneï¿½ï¿½(ï¿½ï¿½) ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Óµï¿?
