2ee7c3d8e313cb53e9a5ad29cc06f12780953880
joker1092 <joker1092@nate.com>
2024-05-21 11:10:02 +0900

    스타트 화면수정
    인풋창 제거 소스 정리

--- GameMain/GDIGameTemplate/Scene/StartScene.cpp
+++ GameMain/GDIGameTemplate/Scene/StartScene.cpp
@@ -1,6 +1,5 @@
 #include "StartScene.h"
 #include "../System/InputSystem.h"
-
 #include "../GameLogic/Event.h"
 
 
@@ -13,76 +12,69 @@ void StartScene::Start()
 
 void StartScene::Init()
 {	
-
+	//필요 인스턴스 로드
+	CResourceManager* CR = CResourceManager::GetInstance();
 	Ranking* myRang = Game::GameManager::GetInstance()->m_Ranking;
+
+	//랭크 불러오기
 	myRang->sortRank();
 	myRang->rankToStr();
-	CResourceManager* CR = CResourceManager::GetInstance();
+
+	//배경
 	myBitmap = CR->LoadBitmapResouce(L"image1",L"startback.png");
 	UIImage* myBack = new UIImage(); // 객체 테스트 
 	myBack->Init(myBitmap, { 640.f,360.f });
 	
-	/*SelectScnEvent* a = new SelectScnEvent(3);
-	delete a;*/
-	/*Gdiplus::Bitmap* startBtn = CR->LoadBitmapResouce(L"startBtn", L"startbtn_sample.bmp");
-	Gdiplus::Bitmap* exitBtn = CR->LoadBitmapResouce(L"exitBtn", L"exitbtn_sample.bmp");*/
-
-	//타이틀
+	//타이틀 //70 , 48 //544,144
 	Gdiplus::Bitmap* titleBitmap = CR->LoadBitmapResouce(L"title", L"titled_sample.bmp");
 	UIImage* titleImg = new UIImage(); // 객체 테스트 
-	////70 , 48
-	////544,144
 	titleImg->Init(titleBitmap, { 70 + (544 / 2),48 + (144 / 2) });
 
-	SelectScnEvent* e_nextScn = new SelectScnEvent((UINT)SceneType::INTRO,mySound::eSoundList::Button); // 씬전환 이벤트 테스트 버튼
+	//이벤트
+	SelectScnEvent* e_nextScn = new SelectScnEvent((UINT)SceneType::INTRO, mySound::eSoundList::Button); // 씬전환 이벤트 테스트 버튼
+	ExitEvent* e_exit = new ExitEvent(mySound::eSoundList::Button);
+
+	//시작버튼 
 	UIButton* gameStartButton = new UIButton(Vector2{ 70 + (200 / 2),254 + (50 / 2) }, e_nextScn, L"UI_Button_Title_StartGame",L".png"); // 객체 테스트
-	
+	//종료버튼
+	UIButton* exit = new UIButton(Vector2{ 70 + (200 / 2),472 + (50 / 2) }, e_exit, L"UI_Button_Title_GameOver", L".png");
+	//조작
+	UIButton* controll = new UIButton(Vector2{ 70 + (200 / 2),363 + (50 / 2) }, nullptr, L"UI_Button_Title_ToMove", L".png"); 
+
+	//랭크보드
 	scoreRect = { 400,200,800,800 };
-	
 	WCHAR _str[500];
 	memset(_str, L'\0', 500);
-	//WCHAR t_str[] = L"랭킹 보드 구현할 장소\n 불러오는건 사이즈 보고 구현할것";
+	
 	std::string myRank;
 	int fromRank = strlen(Game::GameManager::GetInstance()->m_Ranking->str_rank.c_str());
 	myRank.assign(Game::GameManager::GetInstance()->m_Ranking->str_rank);
-	
 	int sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, &myRank[0], (int)myRank.size(), NULL, 0);
 	MultiByteToWideChar(CP_UTF8, 0, &myRank[0], fromRank, &_str[0], sizeNeeded);
 	UIDialog* rankDialog = new UIDialog();
 	rankDialog->Init({ 752,48 }, { 432,624 }, _str);
-
-	ExitEvent* e_exit = new ExitEvent(mySound::eSoundList::Button);
-	//UIButton* exit = new UIButton(Vector2{ 200.0f,600.0f }, e_exit, exitBtn);
-	UIButton* exit = new UIButton(Vector2{ 70 + (200 / 2),472 + (50 / 2) }, e_exit, L"UI_Button_Title_GameOver", L".png");
-	UIInputField* inputField = new UIInputField({500.f, 500.f}, 200.f, 150.f);
-
-	//조작
-	//UIButton* exit = new UIButton(Vector2{ 70 + (200 / 2),472 + (50 / 2) }, e_exit, exitBtn);
-	UIButton* controll = new UIButton(Vector2{ 70 + (200 / 2),363 + (50 / 2) },nullptr,L"UI_Button_Title_ToMove",L".png");\
 	
+	//화면전환
+	UICrossDissolve* backEffect = new UICrossDissolve({ 640.f, 360.f }, Game::GameManager::GetInstance()->sceneBitmap);
+
+	//오브젝트 등록
 	AddObject(myBack);
 	AddObject(titleImg);
 	AddObject(gameStartButton);
 	AddObject(controll);
 	AddObject(exit);
+	AddObject(rankDialog);
+	AddObject(backEffect);
+
+	//이벤트 등록
 	AddEvent(e_nextScn);
-	
 	AddEvent(e_exit);
 
-	AddObject(inputField);
-	
-	UICrossDissolve* backEffect = new UICrossDissolve({ 640.f, 360.f }, Game::GameManager::GetInstance()->sceneBitmap);
-	AddObject(backEffect);
-	
+	//화면 알파 값 초기화
 	alpha = 1.0f;
-
-	AddObject(rankDialog);
-	//Test-------------end
-
 }
 
 StartScene::~StartScene() {
-	//delete[] _str;
 }
 
 void StartScene::Exit() {
